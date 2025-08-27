0e6be2871503192774a8d0863ead26e09b606009
Tanjib-Alamin <tanjibalamin99@gmail.com>
2025-08-27 23:21:22 +0600

    repo, project and 1st problem  done

---
+++ Codeforces_100_Problems/Problem1_10/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// problem_1 : https://codeforces.com/problemset/problem/71/A
+
+
+using System.Linq.Expressions;
+Console.Write("Please give an Input :");
+string input = Console.ReadLine();
+
+int count = input.Length;
+int c = count - 2;
+Console.WriteLine(input.Substring(0,1)+count.ToString()+input.Substring(count-1));
+
+
+char[] arr= new char[count];
+for (int i = 0; i < count; i++)
+{
+    arr[i] = input[i];
+}
+for (int i = 0;i < count; i++)
+{
+   if(i==0) Console.Write(arr[i]);
+   if(i==1) Console.Write(count);
+   if (i == count-1) Console.Write(arr[i]);
+}
+Console.WriteLine();
+for (int i = 0;i<input.Length ; i++)
+{
+
+    if (i == 0) Console.Write(input[i]);
+    if (i == 1) Console.Write(count);
+    if (i == count - 1) Console.Write(input[i]);
+}
